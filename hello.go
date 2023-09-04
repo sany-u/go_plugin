32cd4632bef8d20d5028b989ff198527c178247d
sany-u <142529970+sany-u@users.noreply.github.com>
2023-09-04 18:11:53 +0200

    Update hello.go

--- hello.go
+++ hello.go
@@ -9,7 +9,7 @@ import (
 func Hello(name string) string {
 	// If no name was given, return an error with a message.
 	if name == "" {
-		return "", errors.New("error 001: empty name")
+		return errors.New("error 001: empty name")
 	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
