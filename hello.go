e58cd14daf43df642867a07bde5c8ccd744ba0ed
sany-u <142529970+sany-u@users.noreply.github.com>
2023-09-04 18:06:24 +0200

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,9 +1,16 @@
 package hello
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("error 001: empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
